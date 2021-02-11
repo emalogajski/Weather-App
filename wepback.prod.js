92a3fdbbf63890be647e49a0b62530719aa240c5
Emilija Malogajski <emalogajski@gmail.com>
2021-02-11 06:54:08 +0100

    Adding webpack and loaders and plugins for sass

---
+++ wepback.prod.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './Website/app.js',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: '/.js$/',
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./Website/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({ filename: "style.css" })
+  ]
+}
